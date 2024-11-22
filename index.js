5313694c76bc3e24404ed141922b3e4cb48245d8
Bidyut kundu <106534797+bidyut10@users.noreply.github.com>
2024-11-23 02:36:51 +0530

    login apis

--- index.js
+++ index.js
@@ -45,8 +45,9 @@ app.get('/auth/github/callback', passport.authenticate('github', { session: fals
     res.cookie('token', token, {
         httpOnly: true,  // Make the cookie inaccessible to JavaScript (for security)
         secure: true,  // Use secure cookies in production
-        sameSite: 'Strict',  // Restrict cross-site requests
+        sameSite: 'None',  // Restrict cross-site requests
         maxAge: 3600000,  // 1 hour expiration
+        domain: 'https://githublogin.onrender.com',
     });
 
     // Redirect the user to the home page
